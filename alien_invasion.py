d07638c19f214267cacebe90fcb602b92b056ea1
aaronsjr <99124070+aaronsjr@users.noreply.github.com>
2022-05-07 14:39:02 +0100

    Added some more code to the main file

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,9 +15,14 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get()
-            if event.type == pygame.QUIT:
-                sys.exit()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
