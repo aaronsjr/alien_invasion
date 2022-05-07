62d54529df8184a18462833fb2bdc3eee4bf834d
aaronsjr <99124070+aaronsjr@users.noreply.github.com>
2022-05-07 16:03:45 +0100

    Fixed an incorrect indent on the ship.py file.

--- ship.py
+++ ship.py
@@ -11,3 +11,9 @@ class Ship:
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
