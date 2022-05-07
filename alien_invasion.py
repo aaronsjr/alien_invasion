7be122f035c24f21d46717e0ac8ea83a517f6004
aaronsjr <99124070+aaronsjr@users.noreply.github.com>
2022-05-07 14:15:59 +0100

    Started writing the project code

---
+++ alien_invasion.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        "Initialize the game and create game resources."
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get()
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
