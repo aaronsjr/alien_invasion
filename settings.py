d3efe7efca3acd75fab2afca2457fe7086b283d3
aaronsjr <99124070+aaronsjr@users.noreply.github.com>
2022-06-16 14:17:25 +0100

    Fine tuned game settings.

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
         self.FPS = 75
         
         # Ship settings
-        self.ship_speed = 3
+        self.ship_speed = 7
         self.ship_limit = 3
 
         # Bullet settings
@@ -21,7 +21,7 @@ class Settings:
         self.bullets_allowed = 5
 
         # Alien settings
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.alien_speed = 3.0
+        self.fleet_drop_speed = 20
         # Fleet direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
