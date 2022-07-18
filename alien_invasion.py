1676ca7180fb4a1a9e9052d98eb9d5f3e3aceea9
aaronsjr <aaronsjrlee@gmail.com>
2022-07-18 14:17:16 +0100

    Finished adding the play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,8 +85,27 @@ class AlienInvasion:
                     self._check_keydown_events(event)
                     
                 elif event.type == pygame.KEYUP:
-                    self._check_keyup_events(event)              
+                    self._check_keyup_events(event)
                 
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks the play button."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset game stats
+            self.stats.reset_stats()
+            self.stats.game_active = True             
+            
+            # Remove any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
+
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
         if event.key == pygame.K_RIGHT:
@@ -137,7 +156,7 @@ class AlienInvasion:
         # Determine the number of rows of aliens that fit on the screen.
         ship_height = self.ship.rect.height
         avaliable_space_y = self.actual_screen_height - (3 * alien_height) - ship_height
-        number_rows = avaliable_space_y // (2 * alien_height)
+        number_rows = avaliable_space_y // (2 * alien_height) - 1
 
         # Create the fleet of aliens
         for row_number in range(number_rows):
