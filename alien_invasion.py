48b82fe630eb905019f8544f9e79b001341ab9f4
aaronsjr <aaronsjrlee@gmail.com>
2022-07-19 18:44:47 +0100

    Fixed another bug where the ship will still fire bullets despite the game not being in an active state.

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,10 +81,10 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type == pygame.KEYDOWN:
+                elif event.type == pygame.KEYDOWN and self.stats.game_active == True:
                     self._check_keydown_events(event)
                     
-                elif event.type == pygame.KEYUP:
+                elif event.type == pygame.KEYUP and self.stats.game_active == True:
                     self._check_keyup_events(event)
                 
                 elif event.type == pygame.MOUSEBUTTONDOWN and self.stats.game_active == False:
